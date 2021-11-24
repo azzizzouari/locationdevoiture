94c4ade84fffab3f00a5c2ac77996134264b449f
azzizzouari <92632833+azzizzouari@users.noreply.github.com>
2021-11-24 11:57:57 +0100

    Update javasc.js

--- javasc.js
+++ javasc.js
@@ -3,10 +3,10 @@ function readmore() {
   var btnText = document.getElementById("myBtn");
 
   if (moreText.style.display === "none") {
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  } else {
     btnText.innerHTML = "Read more";
     moreText.style.display = "none";
+  } else {
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
 }
