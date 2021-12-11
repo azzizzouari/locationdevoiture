ef88ac6e362d03f2e0dd636393ecea4bf5152c6f
azzizzouari <92632833+azzizzouari@users.noreply.github.com>
2021-12-11 09:32:48 +0200

    Add files via upload

--- javasc.js
+++ javasc.js
@@ -2,12 +2,55 @@ function readmore() {
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
 
-  if (moreText.style.display === "none"&&btnText.innerHTML === "Lire la suite...") {
+  if (moreText.style.display == "") {
     btnText.innerHTML = "Lire moins";
     moreText.style.display = "inline";
   } 
     else {
     btnText.innerHTML = "Lire la suite...";
-    moreText.style.display = "none";
+    moreText.style.display = "";
   }
+}
+
+function verif_formulaire1() {
+  if(document.getElementById("uname").value == "")  {
+   window.alert("Veuillez entrer votre nom!");
+  }
+   
+  if(documente.getElementById("psw").value == "") {
+    window.alert("Veuillez entrer votre mot de passe!");
+  }
+
+  if(document.getElementById("formulaire").getElementById("psw").value.length("i") < "8"
+  || document.getElementById("psw").value.length("i") > "20"){
+    window.alert("Cette mot de passe n'est pas entre 8 et 20 caracteres!");
+ }
+}
+
+
+function verif_formulaire2() {
+  if(document.getElementById("email").value == "")  {
+    window.alert("Veuillez entrer votre email!");
+   }
+
+  if(document.getElementById("uname").value == "")  {
+    window.alert("Veuillez entrer votre nom!");
+   }
+
+  if(documente.getElementById("psw").value == "") {
+    window.alert("Veuillez entrer votre mot de passe!");
+   }
+  
+  if(document.getElementById("psw").value.length(i) < "8"
+    || document.getElementById(psw).value.length(i) > "20"){
+      window.alert("Cette mot de passe n'est pas entre 8 et 20 caracteres!");
+   }
+
+  if(!(documente.getElementById("psw").value == documente.getElementById("psw-repeat").value)) {
+    window.alert("Les deux Mot de passes ne sont pas identiques");
+   }
+}
+
+function verif() {
+  confirm("êtes-vous sûr?");
 }
